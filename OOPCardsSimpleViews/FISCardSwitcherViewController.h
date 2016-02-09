4acbbc0933fe977de8ec27243de2b7d8a83f1f83
fleish007 <cfleish@gmail.com>
2016-02-09 09:21:34 -0500

    You down with OOP? Oh you know me.

--- OOPCardsSimpleViews/FISCardSwitcherViewController.h
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.h
@@ -9,6 +9,12 @@
 @property (strong, nonatomic) FISCard *fourOfClubs;
 @property (strong, nonatomic) FISCard *eightOfDiamonds;
 @property (strong, nonatomic) FISCard *tenOfHearts;
+@property (nonatomic,retain) IBOutlet UILabel *topLabel;
+@property (nonatomic,retain) IBOutlet UILabel *middleLabel;
+@property (nonatomic,retain) IBOutlet UILabel *bottomLabel;
+
+-(IBAction)action:(id)sender;
+
 
 @end
 
--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -22,4 +22,28 @@
                                                 rank:@"10"];
 }
 
+-(IBAction)action:(id)sender{
+ 
+    UIButton *button = (UIButton*)sender;
+    
+    if (button.tag == 1) {
+        [self.topLabel setText:self.threeOfSpades.cardLabel];
+        [self.middleLabel setText:self.threeOfSpades.cardLabel];
+        [self.bottomLabel setText:self.threeOfSpades.cardLabel];
+    } else if (button.tag == 2){
+        [self.topLabel setText:self.fourOfClubs.cardLabel];
+        [self.middleLabel setText:self.fourOfClubs.cardLabel];
+        [self.bottomLabel setText:self.fourOfClubs.cardLabel];
+    } else if(button.tag == 3){
+        [self.topLabel setText:self.eightOfDiamonds.cardLabel];
+        [self.middleLabel setText:self.eightOfDiamonds.cardLabel];
+        [self.bottomLabel setText:self.eightOfDiamonds.cardLabel];
+    } else if (button.tag == 4){
+        [self.topLabel setText:self.tenOfHearts.cardLabel];
+        [self.middleLabel setText:self.tenOfHearts.cardLabel];
+        [self.bottomLabel setText:self.tenOfHearts.cardLabel];
+    }
+    
+}
+
 @end
